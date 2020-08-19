397d6ec88bb9aa91f264039152e5d0881c0330c0
jamesalee213 <jamesalee213@gmail.com>
2020-08-19 03:33:13 -0700

    Add homebrew

---
+++ homebrew/homebrew.rb
@@ -0,0 +1,35 @@
+
+class Homebrew
+
+    BREWFILE = "~/.dotfiles/homebrew/Brewfile".freeze
+
+    def initialize
+        @installer = HomebrewInstaller.new
+    end
+
+    def run
+        if @installer.should_install
+            puts "Installing Homebrew"
+            @installer.install
+        else
+            puts "Homebrew is already installed"
+        end
+
+        system("brew bundle --file=#{BREWFILE}")
+    end
+end    
+
+class HomebrewInstaller
+    def should_install
+        return `which brew`.to_s.empty?
+    end
+
+    def install
+        system('/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"')
+        puts "Homebrew has successfully been installed"
+    end
+
+    def uninstall
+        system('/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/uninstall.sh)"')
+    end
+end
