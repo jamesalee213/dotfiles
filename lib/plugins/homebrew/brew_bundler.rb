c6665396f2413227360c775f0eb2d1107a48f74a
james.lee2 <james.lee2@workday.com>
2020-08-24 01:31:46 -0700

    Fix brew_bundler bug

--- lib/plugins/homebrew/brew_bundler.rb
+++ lib/plugins/homebrew/brew_bundler.rb
@@ -2,7 +2,7 @@ module BrewBundler
     private 
 
     def brew_bundle(directory)
-        brewfile = brewfile || File.expand_path('Brewfile', directory)
+        brewfile = File.expand_path('Brewfile', directory)
         system("brew bundle --file=#{brewfile}")
     end
 end
