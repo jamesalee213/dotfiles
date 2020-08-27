0aaa2cb5007c81c0aa3537e8e9ab4a1e468bbe85
james.lee2 <james.lee2@workday.com>
2020-08-27 01:39:23 -0700

    Work-in-progress emacs plugin

--- lib/plugins/emacs/emacs_plugin.rb
+++ lib/plugins/emacs/emacs_plugin.rb
@@ -1,4 +1,5 @@
 require_relative "../homebrew/brew_installer"
+require_relative "../../utils/commands/create_symlink_command"
 
 class Emacs
     def initialize(brew_installer = BrewInstaller.new)
@@ -8,13 +9,31 @@ class Emacs
     def plug
         puts "Installing dependencies..."
         @brew_installer.bundle(__dir__)
-        system "ln -s /usr/local/opt/emacs-plus/Emacs.app /Applications/Emacs.app"
+
+        # put a condition
+        #system "ln -s /usr/local/opt/emacs-plus/Emacs.app /Applications/Emacs.app"
 
         puts "Install Doom emacs..."
-        system "git clone https://github.com/hlissner/doom-emacs ~/.emacs.d"
-        system "~/.emacs.d/bin/doom install"
+        # put a condition
+        #system "git clone https://github.com/hlissner/doom-emacs ~/.emacs.d"
+        #system "~/.emacs.d/bin/doom install"
 
         #maybe need add ~/.emacs.d/bin to PATH
+
+        #move the config files for doom
+
+        # Add check for doom?
+        #CreateSymlink.new("#{__dir__}/config.el", "~/.doom.d/config.el")
+        #CreateSymlink.new("#{__dir__}/init.el", "~/.doom.d/init.el")
+        #CreateSymlink.new("#{__dir__}/packages.el", "~/.doom.d/packages.el")
+
+        # Install kotlin-language-server
+        # https://github.com/fwcd/kotlin-language-server/releases/download/0.7.0/server.zip
+        # use unzip to unzip the above file
+
+        # Add a check for emacs?
+        # move and rename to appropriate place: ~/.emacs.d/lsp-servers/kotlin
+        # add the executable to PATH
     end
 
     def unplug
--- lib/plugins_manager.rb
+++ lib/plugins_manager.rb
@@ -3,18 +3,21 @@
 require_relative "plugins/homebrew/homebrew_plugin"
 require_relative "plugins/fish/fish_plugin"
 require_relative "plugins/emacs/emacs_plugin"
+require_relative "plugins/misc/python_plugin"
 
 puts 'hello world'
 
 $plugins = [
     Homebrew.new,
+    Python.new,
     Fish.new,
     Emacs.new
 ]
 
-puts "no plugins installing right now..."
+#puts "no plugins installing right now..."
 
-#plugin = Fish.new
+plugin = Fish.new
 #plugin = Emacs.new
-#plugin.plug
+#plugin = Python.new
+plugin.plug
 #plugin.unplug
